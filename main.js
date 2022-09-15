dcb676708ade9f4bf73efa4933c34c61f3b29b82
pikalupps <putudhikap@gmail.com>
2022-09-15 21:14:32 +0700

    membuat angka 3

--- main.js
+++ main.js
@@ -3,10 +3,47 @@ function main() {
   var kanvas = document.getElementById("kanvas")
 	var gl = kanvas.getContext("webgl")
 
+  // untuk menyimpan array
+	var vertices = [
+    //angka 3
+		-0.9, 0.3,
+    -0.9, 0.6,
+    -0.85, 0.7,
+    -0.55, 0.7,
+    -0.5, 0.6,
+    -0.5, 0.1,
+    -0.53, 0,
+    -0.5, -0.1,
+    -0.5, -0.6,
+    -0.55, -0.7,
+    -0.85, -0.7,
+    -0.9, -0.6,
+    -0.9, -0.3,
+    -0.8, -0.3,
+    -0.8, -0.45,
+    -0.6, -0.45,
+    -0.6, -0.1,
+    -0.75, -0.1,
+    -0.75, 0.1,
+    -0.6, 0.1,
+    -0.6, 0.45,
+    -0.8, 0.45,
+    -0.8, 0.3,
+	]
+
+  var buffer = gl.createBuffer()
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+  
   // vertex shader
   var vertexShaderCode = `
-  void main() {
+  attribute vec2 aPosition;
+	void main() {
+		float x = aPosition.x;
+		float y = aPosition.y;
+		gl_PointSize = 50.0;
 
+		gl_Position = vec4(x, y, 0.0, 1.0);
   }`
   var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER)
   gl.shaderSource(vertexShaderObject, vertexShaderCode)
@@ -14,8 +51,12 @@ function main() {
 
   // fragment shader
   var fragmentShaderCode = `
-  void main(){
-
+  precision mediump float;
+	void main() {
+		float r = 0.46;
+		float g = 0.90;
+		float b = 0.85;
+		gl_FragColor = vec4(r, g, b, 1.0);
   }`
   var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER)
   gl.shaderSource(fragmentShaderObject, fragmentShaderCode)
@@ -28,6 +69,13 @@ function main() {
   gl.linkProgram(shaderProgram)
   gl.useProgram(shaderProgram)
 
-  gl.clearColor(1.0, 0.0, 0.0, 1.0)
+  // mengajari GPU cara mengoleksi nilai posisi dari ARRAY_BUFFER
+	var aPosition = gl.getAttribLocation(shaderProgram, "aPosition")
+	gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0)
+	gl.enableVertexAttribArray(aPosition)
+
+  gl.clearColor(0.02,0.27,0.37, 1.0)
   gl.clear(gl.COLOR_BUFFER_BIT)
+
+  gl.drawArrays(gl.LINE_LOOP, 0, 23)
 }
